5e1280c7a51e81bcb8ce4d8b175d0c394fde62a9
krushndayshmookh <krushndayshmookh@gmail.com>
2017-05-31 09:48:49 +0530

    rename, refactor app is now Clavi

--- www/pages/student-profile/profile.js
+++ www/pages/student-profile/profile.js
@@ -1,15 +1,17 @@
 var uname = Cookies.get("username");
 //console.log(uname);
 
-$(document).ready(function(){
-$.get( hostaddress + "/userdata", {username:uname}, function (data) {
-	console.log(data);
-	for (term in data){
+$(document).ready(function () {
+	$.get(hostaddress + "/userdata", {
+		username: uname
+	}, function (data) {
+		console.log(data);
+		for (term in data) {
 
-		//feild = "#"+term;
-		//console.log(feild);
-		document.getElementById(term).innerHTML = data[term];
-	}
-	//document.getElementById("user-img").setAttribute("src", data["user-img"]);
-});
+			//field = "#"+term;
+			//console.log(field);
+			document.getElementById(term).innerHTML = data[term];
+		}
+		//document.getElementById("user-img").setAttribute("src", data["user-img"]);
+	});
 });
