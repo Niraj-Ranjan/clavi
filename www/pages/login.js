a04e69c214d1d8e97a7b3a971af6545141e68953
krushndayshmookh <krushndayshmookh@gmail.com>
2017-06-10 10:14:23 +0530

    added forgot password handler

--- www/pages/login.js
+++ www/pages/login.js
@@ -1,42 +1,63 @@
 $("#reset-box-btn").click(function () {
-	$("#login-box").slideUp();
-	$("#password-reset-box").slideDown();
-	console.log("hi")
+    $("#login-box").slideUp();
+    $("#password-reset-box").slideDown();
+
 });
 
 $("#reset-cancel-btn").click(function () {
-	$("#login-box").slideDown();
-	$("#password-reset-box").slideUp();
+    $("#login-box").slideDown();
+    $("#password-reset-box").slideUp();
 });
 
 $("#sign-in-btn").click(function () {
-	user = {
-		uname: $("#username").val(),
-		pword: $("#password").val()
-	}
-	$.get(hostaddress + "/login", user, function (data) {
-		console.log(data);
-		if (data == "true") {
-			Cookies.set('username', user.uname, {
-				expires: 7,
-				path: '/'
-			});
+    user = {
+        uname: $("#username").val(),
+        pword: $("#password").val()
+    }
+    $.get(hostaddress + "/login", user, function (data) {
+        //console.log(data);
+        if (data == "true") {
+            Cookies.set('username', user.uname, {
+                expires: 7,
+                path: '/'
+            });
 
-			$.get(hostaddress + "/username-name", {
-				user: user.uname
-			}, function (returnedusername) {
-				Cookies.set("username-name", returnedusername, {
-					expires: 7,
-					path: '/'
-				});
-				window.location = "../home.html";
-			});
+            $.get(hostaddress + "/username-name", {
+                user: user.uname
+            }, function (returnedusername) {
+                Cookies.set("username-name", returnedusername, {
+                    expires: 7,
+                    path: '/'
+                });
+                window.location = "../home.html";
+            });
 
 
-		} else if (data == "false") {
-			alert("Password Incorrect.");
-		} else {
-			alert("Sorry, " + user.uname + " not found.")
-		}
-	});
+        } else if (data == "false") {
+            alert("Password Incorrect.");
+        } else {
+            alert("Sorry, " + user.uname + " not found.")
+        }
+    });
+})
+
+$("#reset-cancel-btn").click(function () {
+    $("#login-box").slideDown();
+    $("#password-reset-box").slideUp();
+});
+
+$("#reset-btn").click(function () {
+
+    $.get(hostaddress + "/passwordreset", {
+        email: $("#reset-email").val()
+    }, function (data) {
+        //console.log(data);
+        if (data == "success") {
+
+            alert("Request sent successfully. You will shortly recieve an email with your credentials.");
+
+        } else {
+            alert("Sorry, e-mail not found. Try again.");
+        }
+    });
 })
