3650fe37878e720b375b83de6fbfd897343e010c
krushndayshmookh <krushndayshmookh@gmail.com>
2018-03-07 17:58:15 +0530

    attendance viewer UI

--- www/pages/academics/attendance.js
+++ www/pages/academics/attendance.js
@@ -9,6 +9,7 @@ function getAttendance() {
         var innerdata = "";
         var total = 0;
         var attendanceLength = 0;
+
         for (var subject in attendancedata) {
 
             var attendancevalue = attendancedata[subject];
@@ -21,28 +22,45 @@ function getAttendance() {
                 subjectcolor = "#dd0000";
             }
 
+            /*
             innerdata = innerdata.concat("<div class='row'><div class='col s12 m3 truncate'>" + subject + "</div><div class='col s12 m9'><div class='graph-bar' style='width:" + attendancevalue + "%;background:" + subjectcolor + "'>" + attendancevalue + "%</div></div></div>");
+            */
+
+            innerdata = innerdata.concat("<div class='row'><div class='col s12 m4 push-m4'><div class='card bottom-padded row hoverable'><div class='graph-bar' style='width:" + attendancevalue + "%;background:" + subjectcolor + "'></div><div class='padded'><div class='col s9  truncate flow-text'>" + subject + "</div><div class='col s3 flow-text'>" + attendancevalue + "%</div></div></div></div></div>");
+
 
             attendanceLength++;
 
             total = total + attendancevalue;
             //console.log(total);
         }
+
         //console.log(innerdata);
         document.getElementById("attendance-graph-cotainer").innerHTML = innerdata;
 
-        var average = total / attendanceLength;
+
+
+        var average = Math.floor(total / attendanceLength);
         var attendanceStatus = "";
-        //console.log(total);
-        //console.log(attendanceLength);
-        //console.log(average);
+
+        //var avgElems = document.getElementsByClassName("avg-color");
+        //console.log(avgElems);
 
         if (average >= 75) {
             attendanceStatus = "good";
-        } else {
+        } else if (average >= 65) {
             attendanceStatus = "poor";
+            $('.avg-color').removeClass("blue");
+            $('.avg-color').addClass("yellow");
+        } else {
+            attendanceStatus = "very poor";
+            $('.avg-color').removeClass("blue");
+            $('.avg-color').addClass("red");
         }
+
         document.getElementById("attendance-status").innerHTML = attendanceStatus;
+        document.getElementById("avg-percentage").innerHTML = average + "%";
+
         hideWait();
     });
 }
