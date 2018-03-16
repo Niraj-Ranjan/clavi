c9995fc6e9d499b3dc504b87627c2aa1e325f605
krushndayshmookh <krushndayshmookh@gmail.com>
2018-03-17 05:07:55 +0530

    added cancel btn

--- www/pages/canteen/js/canteen.js
+++ www/pages/canteen/js/canteen.js
@@ -29,6 +29,20 @@ function updatetotal() {
     document.getElementById("total-amt").innerHTML = finaltotal;
 }
 
+function removeitem(itemid) {
+
+    for (var item in orderlist) {
+        if (orderlist[item]["d"] = itemid) {
+            finaltotal = finaltotal - orderlist[item]["quantity"] * orderlist[item]["rate"];
+            orderlist.splice(item, 1);
+            $("#" + itemid).remove();
+            updatetotal();
+            break;
+        }
+    }
+    //element.parentNode.removeChild(element);
+}
+
 
 function ordersend(itemconfirm) {
     orderobject.quantity = parseInt($("#order-quantity").val(), 10);
@@ -43,8 +57,9 @@ function ordersend(itemconfirm) {
         $("#order-modal").modal('close');
 
         finaltotal = finaltotal + orderobject.quantity * orderobject.rate;
+        orderobject.d = orderobject.item + orderobject.quantity;
 
-        var elem = "<div class='collection-item row'><div class='col s8'><p>" + orderobject.item + "<p></div><div class='col s4'><p class='right'>" + orderobject.quantity * orderobject.rate + "</p></div></div>";
+        var elem = "<div class='collection-item row' id='" + orderobject.d + "'><div class='col s7'><p>" + orderobject.item + "<p></div><div class='col s3'><p class='center'>" + orderobject.quantity * orderobject.rate + "</p></div><div class='col s2'><a class='center red white-text btn-floating' onclick='removeitem(" + '"' + orderobject.d + '"' + ")'><i class='material-icons'>close</i></a></div></div>";
 
         $("#cart-items").append(elem);
 
@@ -78,6 +93,12 @@ function orderconfirm() {
     }, function (returnedstring) {
         //console.log(orderlist);
         if (returnedstring == "success") {
+
+            document.getElementById("cart-items").innerHTML = "";
+            orderobject = {};
+            orderlist = [];
+            finaltotal = 0;
+            updatetotal();
 
             Materialize.toast("Order sent!", 3000);
 
@@ -127,50 +148,51 @@ function getcanteen() {
 
     showWait();
     $.get(hostaddress + "/rates", function (rates) {
-            //console.log(rates);
-            $.get(hostaddress + "/canteen/data/available.json", function (availableitems) {
-                    //console.log(availableitems);
-                    for (var category in availableitems) {
-                        var innerdata = "";
-                        //console.log(rates[category]);
+        //console.log(rates);
+        $.get(hostaddress + "/canteen/data/available.json", function (availableitems) {
+            //console.log(availableitems);
+            for (var category in availableitems) {
+                var innerdata = "";
+                //console.log(rates[category]);
 
-                        for (var item in availableitems[category]) {
+                for (var item in availableitems[category]) {
 
-                            var itemname = availableitems[category][item];
-                            //console.log(itemname);
-                            var imagelink = hostaddress + "/canteen/images/" + category + "/" + itemname + ".png";
-                            innerdata = innerdata.concat("<div class='col s12 m3'><div class='card'><div class='card-image tiny'><img src='" + imagelink + "'></div><div class='card-content'><span class='card-title truncate'>" + itemname + "</span><p> I am some description about " + itemname + ".</p></div><div class='card-action valign-wrapper'><div class='btn right red white-text' onclick='orderthis(" + '"' + itemname + '"' + "," + '"' + category + '"' + "," + rates[category][itemname] + "," + '"' + imagelink + '"' + ")'>Rs. " + rates[category][itemname] + "</div></div></div></div>");
-                            }
-                            //console.log(category);
-                            document.getElementById(category).innerHTML = innerdata;
-                        }
+                    var itemname = availableitems[category][item];
+                    //console.log(itemname);
+                    var imagelink = hostaddress + "/canteen/images/" + category + "/" + itemname + ".png";
+                    innerdata = innerdata.concat("<div class='col s12 m3'><div class='card'><div class='card-image tiny'><img src='" + imagelink + "'></div><div class='card-content'><span class='card-title truncate'>" + itemname + "</span><p> I am some description about " + itemname + ".</p></div><div class='card-action valign-wrapper'><div class='btn right red white-text' onclick='orderthis(" + '"' + itemname + '"' + "," + '"' + category + '"' + "," + rates[category][itemname] + "," + '"' + imagelink + '"' + ")'>Rs. " + rates[category][itemname] + "</div></div></div></div>");
+                }
+                //console.log(category);
+                document.getElementById(category).innerHTML = innerdata;
+            }
 
-                    });
+        });
 
-                /*
-                $.get(hostaddress + "/canteen/data/trending.json", function (trendingitems) {
-                    //console.log(availableitems);
-                    var innerdata = "";
-                    for (var itemname in trendingitems) {
-
-
-                        var itemcategory = trendingitems[itemname];
-                        //console.log(itemname);
-                        var imagelink = hostaddress + "/canteen/images/" + itemcategory + "/" + itemname + ".png";
+        /*
+        $.get(hostaddress + "/canteen/data/trending.json", function (trendingitems) {
+            //console.log(availableitems);
+            var innerdata = "";
+            for (var itemname in trendingitems) {
 
 
-                        innerdata = innerdata.concat("<div class='col s6 m3'><div class='card'><div class='card-image tiny'><img src='" + imagelink + "'><span class='card-title text-shadow truncate'>" + itemname + "</span></div><div class='card-action'><a href='#!' onclick='orderthis(" + '"' + itemname + '"' + "," + '"' + itemcategory + '"' + "," + rates[itemcategory][itemname] + "," + '"' + imagelink + '"' + ")'>Rs. " + rates[itemcategory][itemname] + "</a></div></div></div>");
-
-                    }
-                    document.getElementById("trending").innerHTML = innerdata;
-
-                });
-
-                */
-            }); hideWait();
-    }
+                var itemcategory = trendingitems[itemname];
+                //console.log(itemname);
+                var imagelink = hostaddress + "/canteen/images/" + itemcategory + "/" + itemname + ".png";
 
 
-    $(document).ready(function () {
-        getcanteen();
+                innerdata = innerdata.concat("<div class='col s6 m3'><div class='card'><div class='card-image tiny'><img src='" + imagelink + "'><span class='card-title text-shadow truncate'>" + itemname + "</span></div><div class='card-action'><a href='#!' onclick='orderthis(" + '"' + itemname + '"' + "," + '"' + itemcategory + '"' + "," + rates[itemcategory][itemname] + "," + '"' + imagelink + '"' + ")'>Rs. " + rates[itemcategory][itemname] + "</a></div></div></div>");
+
+            }
+            document.getElementById("trending").innerHTML = innerdata;
+
+        });
+
+        */
     });
+    hideWait();
+}
+
+
+$(document).ready(function () {
+    getcanteen();
+});
