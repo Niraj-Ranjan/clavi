4670c05b10ae2ee63246fa11948ba6c7469dd9d8
krushndayshmookh <krushndayshmookh@gmail.com>
2018-03-31 02:33:13 +0530

    host

--- www/js/script.js
+++ www/js/script.js
@@ -2,9 +2,9 @@
 
 
 
-var hostaddress = "http://clavi.herokuapp.com";
+//var hostaddress = "http://clavi.herokuapp.com";
 //const hostaddress = "http://192.168.225.68:3000";
-//const hostaddress = "http://localhost:3000";
+const hostaddress = "http://localhost:3000";
 
 
 
