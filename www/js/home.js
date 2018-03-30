2312085dd3953042978d8339c167c3ecfb24a7b4
krushndayshmookh <krushndayshmookh@gmail.com>
2018-03-31 04:12:54 +0530

    veiw scholarship

--- www/js/home.js
+++ www/js/home.js
@@ -13,10 +13,61 @@ $(document).ready(function () {
         });
         $('#menu-btn-tap-target').tapTarget('open');
     }
+
+    getScholarship("mahadbt");
+
+    $('.collapsible').collapsible({
+        accordion: false
+    });
+
 });
 
 
+function getScholarship(govt) {
+    $.get(hostaddress + "/scholarships", {
+        gov: govt
+    }, function (scholarship) {
 
+        for (var notice in scholarship) {
+
+
+            /*
+                        var innerDataStart = "<li class='rounded'><div class='collapsible-header'><div class='truncate flow-text'>" + subject + "</div></div><div class='collapsible-body hidden-overflow'>";
+
+                        var innerDataMiddle = "";
+                        totalMarks = 0;
+                        for (var type in academicsdata[subject].marks) {
+                            innerDataMiddle = innerDataMiddle.concat("<div class='row valign-wrapper'><div class='col s1'>" + type.toUpperCase() + "</div><div class='col s11'><ol>");
+                            for (var mark in academicsdata[subject].marks[type]) {
+                                innerDataMiddle = innerDataMiddle.concat("<li style='display: inline-block;'><div class='circle-highlight valign-wrapper'><div class='container center'>" + academicsdata[subject].marks[type][mark] + "</div></div></li>");
+
+                                //totalMarks = totalMarks + academicsdata[subject].marks[type][mark];
+                            }
+                            innerDataMiddle = innerDataMiddle.concat("</ol></div></div>");
+                        }
+                        //innerDataMiddle = innerDataMiddle.concat("<div class='row'>total<div class='circle-highlight valign-wrapper'><div class='container center'>" + totalMarks + "</div></div></div>");
+                        innerDataMiddle = innerDataMiddle.concat("</div>");
+                        var innerDataEnd = "</li>";
+                        var innerdata = innerDataStart + innerDataMiddle + innerDataEnd;
+                        */
+
+            var innerdata = "<li><div class='collapsible-header'><div class='truncate flow-text'>" + scholarship[notice]["SchemeName"] + "</div></div><div class='collapsible-body hidden-overflow'>";
+
+            innerdata = innerdata.concat(scholarship[notice]["overview"] );
+
+
+            innerdata = innerdata.concat("</div></li>")
+
+            $("#scholarship-container").append(innerdata);
+
+
+
+
+
+        }
+
+    });
+}
 
 
 
